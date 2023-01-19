c21ac94320e0af01952222533543dc58c0523780
Rob Cavanaugh <46727239+rbcavanaugh@users.noreply.github.com>
2023-01-19 14:17:36 -0500

    getFI initial

---
+++ R/getFI.R
@@ -0,0 +1,96 @@
+
+#' Title
+#'
+#' @param .data data.frame; FI dataframe of all data (e.g., efi_dat.csv)
+#' @param index chr; name of FI index (efi, vafi, hfrs)
+#' @param .data_search data.frame; dataframe of participant IDs, start, and end dates to be searched
+#' @param person_id chr; column name of person_id within .data_search dataframe
+#' @param start_date chr; column name of start date column within .data_search. YYYY-MM-DD
+#' @param end_date chr; column name of end date column within .data_search. YYYY-MM-DD
+#' @param summary log; whether to return summary data or results broken out by FI concept
+#' @param group_var chr; whether to summarize by "person" (default) or "category"
+#'
+#' @return
+#' @export
+#'
+#' @examples \dontrun{
+#' getFI(.data = efi_dat,
+#' index = "efi",
+#' .data_search = test_dat,
+#' person_id = "person_id",
+#' start_date = "start_date",
+#' end_date = "end_date",
+#' group_var = "person_id",
+#' summary = TRUE)
+#' }
+getFI = function(.data, index,
+                 .data_search, person_id, start_date, end_date,
+                 summary, group_var){
+
+    if(group_var != "person_id" & group_var != "category"){
+        stop("OOPS! please select an allowable grouping variable")
+    }
+
+    index_var = case_when(
+        index == "efi" | index == "efragicap" ~ "efi",
+        index == "vafi" ~ "vafi",
+        index == "hfrs" ~ "hfrs",
+        TRUE ~ NA_character_
+    )
+
+    if(paste(index_var, "category", sep = "_")  %in% colnames(.data)){
+        cat("Data and selected index are compatible \n")
+    } else {
+        stop("OOPS! data and selected index are mismatched")
+    }
+
+    pid = .data_search %>%
+        select(person_id = !!person_id,
+               start_date = !!start_date,
+               end_date = !!start_date) %>%
+        mutate(search_interval = interval(ymd(start_date), ymd(end_date)))
+
+
+
+    cat("Searching for matching cases ... \n")
+
+    # create a temporary data frame
+    # fix the start and end date columns
+    # rename columns to be generic
+    # keep only rows within the interval
+    tmp = .data %>%
+        filter(person_id %in% pid$person_id) %>%
+        mutate(date = ym(paste(start_year, start_month, sep = "-")),
+               obs = 1) %>%
+        select(person_id,
+               category = paste(!!index_var, "category", sep = "_"),
+               date,
+               score = ifelse(!!index_var == "hfrs", paste(!!index_var, "score", sep = "_"), "obs")
+        ) %>%
+        left_join(pid, by = "person_id") %>%
+        filter(date %within% search_interval)
+
+    # if its a summary dataframe, summarize by person or category
+    # HFRS adds up the scores, otherwise we're just counting rows
+
+    cat("Generating distinct occurances for person_id/category combo... \n")
+    tmp = tmp %>%
+        distinct(person_id, category, .keep_all = TRUE)
+
+    if(isTRUE(summary)){
+        cat("Summarizing data ... \n")
+        tmp = tmp %>%
+            group_by(.data[[group_var]]) %>%
+            summarize(FI = sum(score)) %>%
+            arrange(desc(FI))
+    } else {
+        cat("Note: grouping variable ignored when summary = FALSE \n")
+        tmp = tmp %>%
+            select(person_id, category, score) %>%
+            arrange(person_id)
+    }
+
+    cat("Success!")
+    return(tmp)
+
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# aouRoux
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+R package of helper functions for the Roux OHDSI Frailty project
+
+## Installation
+
+You can install the development version of aouRoux like so:
+
+``` r
+remotes::install_github("rbcavanaugh/aouRoux)
+```
+
+## Current list of functions
+
+- `getFI()` returns fralty scores for a several FIs given a dataframe of
+  person_id’s, start, and end dates.
