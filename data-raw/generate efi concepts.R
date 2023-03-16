ed7323c0aaaf38430c5a3ebfe4ea4bb1e46301ff
Rob Cavanaugh <46727239+rbcavanaugh@users.noreply.github.com>
2023-03-16 15:04:41 -0400

    test

--- data-raw/generate efi concepts.R
+++ data-raw/generate efi concepts.R
@@ -1,6 +1,7 @@
 # efi concepts
 
-efi_concepts = read.csv("~/Downloads/efi_scores_dictionary.csv")
+efi_concepts = read.csv("~/Downloads/efi_scores_dictionary.csv") %>%
+    rename(category = efi_category, concept_id = efi_concept_id)
 
 usethis::use_data(efi_concepts, overwrite = TRUE)
 
