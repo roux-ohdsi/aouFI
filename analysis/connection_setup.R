996992e0c4c850d36eccf4ce2aff70a495ab13bb
Cavanaugh <r.cavanaugh@northeastern.edu>
2024-04-02 16:08:09 -0400

    fix efi

--- analysis/connection_setup.R
+++ analysis/connection_setup.R
@@ -26,7 +26,9 @@ con =  DatabaseConnector::connect(dbms = "redshift",
                                   server = "ohdsi-lab-redshift-cluster-prod.clsyktjhufn7.us-east-1.redshift.amazonaws.com/ohdsi_lab",
                                   port = 5439,
                                   user = keyring::key_get("lab_user"),
-                                  password = keyring::key_get("lab_password"))
+                                  password = keyring::key_get("lab_password"),
+                                  pathToDriver = "D:/Users/r.cavanaugh/Documents"
+                                  )
 
 
 # check connection
